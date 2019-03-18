3b7ac4c2c630d6b7e1843598ba8b51c7900bd6b6
flaviohsm0506 <flaviohsm0506@gmail.com>
2019-03-18 11:19:30 -0300

    Estado React

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
 
 import Label from "../Label";
+import Input from "../Input";
+import GenderImage from "../GenderImage";
 
 class NewUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: "Minora"
+      },
+      validation: {
+        invalidName: false
+      }
+    };
+  }
+
+  updateUserName = event => {
+    let user = this.state.user;
+    user.name = event.target.value;
+    this.setState({
+      user: user
+    });
+  };
+
   render() {
     return (
       <div className="center">
         <form className="pure-form pure-form-stacked">
-          <Label htmlFor="name" text="Quem é você?" />
+          <Label
+            htmlFor="name"
+            text="Quem é você?"
+            invalidValue={this.state.validation.invalidName}
+          />
+          <Input
+            id="name"
+            placeholder="Digite seu nome"
+            maxLength="40"
+            readOnly={false}
+            invalidValue={this.state.validation.invalidName}
+            defaultValue={this.state.user.name}
+            onChange={this.updateUserName}
+          />
+          <GenderImage gender="m" />
+          <GenderImage gender="f" />
         </form>
       </div>
     );
